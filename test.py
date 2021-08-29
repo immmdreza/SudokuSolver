ec13c7100d29744df05c433b7f3fe5dab860929b
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2021-08-29 21:23:52 +0430

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+from sudoku_ import Sudoku_s
+from solver_ import SudokuSolver_s
+
+puzzle = [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+sudoku_s = Sudoku_s(puzzle)
+solver_s = SudokuSolver_s(sudoku_s)
+
+if solver_s.full_solve():
+    print(sudoku_s)
